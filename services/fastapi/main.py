a8e0a58fe9b24f37a5c2327866ff128ae58c89a4
martinfg <martin.george@posteo.de>
2022-07-26 18:00:36 +0200

    add fastapi

---
+++ services/fastapi/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(docs_url='/docs', openapi_url='/openapi.json')
+
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+@app.get('/')
+def get_root():
+  return {'message': 'Bike Prediction API'}
+
+# @app.get(f'{subpath}/detect_lang/')
+# async def detect_language(text: str):
+# 	return detector.detect_language(text)
